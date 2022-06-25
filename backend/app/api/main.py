a28ec75808db202fc97be224e17ed6745f29042e
Selamet Şamlı <selametsamli@gmail.com>
2022-06-25 16:40:27 +0300

    Alembic settings

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -5,18 +5,18 @@ from app.api.routes import router as api_router
 
 
 def get_application():
-    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
-    app.add_middleware(
+    application = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
+    application.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    app.add_event_handler("startup", tasks.create_start_app_handler(app))
-    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
-    app.include_router(api_router, prefix="/api")
-    return app
+    application.add_event_handler("startup", tasks.create_start_app_handler(application))
+    application.add_event_handler("shutdown", tasks.create_stop_app_handler(application))
+    application.include_router(api_router, prefix="/api")
+    return application
 
 
 app = get_application()
