59c8320a3f49297ed1dfe8ed90c8872b8fe9fe08
selametsamli <selamet96@gmail.com>
2024-11-20 21:06:40 +0300

    Create custom user model

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('company',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('company',)}),
+    )
+    list_display = ['username', 'email', 'company', 'is_staff']
+    search_fields = ['company']
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    company = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.company})"
